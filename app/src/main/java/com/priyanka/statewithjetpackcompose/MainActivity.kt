9e6456d1cf813db54956d1c4fc53e4d781f4e27c
priyanka-bhoir <er.priyankabhoir1@gmail.com>
2022-05-21 18:10:39 +0530

    updated README.md

--- app/src/main/java/com/priyanka/statewithjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/priyanka/statewithjetpackcompose/MainActivity.kt
@@ -7,9 +7,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
