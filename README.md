23376199b0e5b39229c2fbe85ded4d32b14d07f7
priyanka-bhoir <er.priyankabhoir1@gmail.com>
2022-05-21 18:07:00 +0530

    updated README.md

--- README.md
+++ README.md
@@ -3,9 +3,20 @@
 #### used to interact with UI componets
 #### ReDrawing of UI componets is called Recomposing in JetPack Compose
 
+```
+here i have created a simple box clicking on that you can change the bacground color of the box.
+```
+
 ## Key Learnings from this using jetpackComposeLearning
 
-
+## How it Works
+``
+when you click on the screen background color changes with state
+``
 ## Output
+![img.png](img.png)
+![img_1.png](img_1.png)
 
 ## Learning Source
+
+
--- app/src/main/java/com/priyanka/statewithjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/priyanka/statewithjetpackcompose/MainActivity.kt
@@ -3,28 +3,52 @@ package com.priyanka.statewithjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.priyanka.statewithjetpackcompose.ui.theme.StateWithJetPackComposeTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            StateWithJetPackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-
-                }
-            }
+            ColorBox(Modifier.fillMaxSize())
         }
+    }
+}
+
+@Composable
+fun ColorBox(modifier: Modifier = Modifier) {
+    val color = remember {
+        mutableStateOf(Color.Yellow)
+    }
+    Box(modifier = modifier
+        .background(color.value)
+        .clickable {
+            color.value = Color(
+                Random.nextFloat(),
+                Random.nextFloat(),
+                Random.nextFloat(),
+                1f
+            )
+        })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    StateWithJetPackComposeTheme {
+        ColorBox(Modifier.fillMaxSize())
     }
 }
